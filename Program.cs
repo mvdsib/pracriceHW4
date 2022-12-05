de544ea3f3b8905d0f4894acfb4b89afc53681c1
Vitalii <mvdsib@gmail.com>
2022-12-05 22:28:19 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+string s_N = Console.ReadLine();
+int N = Convert.ToInt32(s_N);
+
+int i = 0;
+for(i = 1; i <= N; i++)
+{
+if(i % 2 == 0)
+{
+    System.Console.WriteLine($"i = {i} ");
+}
+else
+{
+    Console.WriteLine("Нет подходящих значений");  
+}
+}
+
